ac10686057a93f15d97706bc1d0487020d2e9da9
sabuto <robe_dunne@hotmail.com>
2020-02-19 20:08:03 +0000

    update config and run

--- etesync/run.sh
+++ etesync/run.sh
@@ -1,2 +1,20 @@
 #!/usr/bin/env bashio
-echo "hello World"
+
+DB_PATH=$(bashio::config 'db_path')
+SUPER_USER=$(bashio::config 'super_user')
+SUPER_PASS=$(bashio::config 'super_pass')
+SUPER_EMAIL=$(bashio::config 'super_email')
+# shellcheck disable=SC2198
+if [ -n "$@" ]; then
+  exec "$@"
+fi
+
+if ! bashio::fs.file_exists "${DB_PATH}}"; then
+  bashio::log.info 'First Run, Setting up DB'
+  "$BASE_DIR"/manage.py migrate
+
+  if [ "$SUPER_USER" -a "$SUPER_PASS" ]; then
+      bashio::log.info 'Creating Super User'
+      echo "from django.contrib.auth.models import User; User.objects.create_superuser('$SUPER_USER' , '$SUPER_EMAIL', '$SUPER_PASS')" | python manage.py shell
+  fi
+fi
