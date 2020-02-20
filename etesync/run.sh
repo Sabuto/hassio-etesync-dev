c235a43cc12a07f170814b2163c9b08fd69f4efa
Sabuto <robe_dunne@hotmail.com>
2020-02-20 09:23:22 +0000

    m

--- etesync/run.sh
+++ etesync/run.sh
@@ -19,7 +19,7 @@ if ! bashio::fs.file_exists "${DB_PATH}"; then
   echo "from django.contrib.auth.models import User; User.objects.create_superuser('$SUPER_USER' , '$SUPER_EMAIL', '$SUPER_PASS')" | python manage.py shell
 fi
 
-if ! bashio::fs.directory_exists "${STATIC_DIR}/static/admin"; then
+if ! bashio::fs.directory_exists '"${STATIC_DIR}"/static/admin'; then
   bashio::log.info "Static files are missing, lets do something about that...."
   mkdir -p "$STATIC_DIR/static"
   "$BASE_DIR"/manage.py collectstatic
